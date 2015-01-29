b33f28b15cb78e8a28c85d09cbdcc59ee557f0f6
GreatVV <marinegreatvv@yahoo.com>
2015-01-29 22:25:09 +0300

    Figure stacking

--- Assets/Scripts/Game/Field.cs
+++ Assets/Scripts/Game/Field.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Field : MonoBehaviour
@@ -20,7 +21,7 @@ public class Field : MonoBehaviour
     {
         ActiveShape = ShapeFactory.CreateRandom();
         ActiveShape.transform.SetParent(transform);
-        ActiveShape.transform.position = Position + new Vector2(Size.x/2, Size.y);
+        ActiveShape.transform.position = Position + new Vector2(Size.x / 2, Size.y);
     }
 
     public void Start()
@@ -40,24 +41,61 @@ public class Field : MonoBehaviour
         }
         else
         {
+            var newSquares = ActiveShape.Squares;
+            foreach (var newSquare in newSquares)
+            {
+                var position = newSquare.transform.position;
+                newSquare.transform.position = new Vector3(
+                    Mathf.RoundToInt(position.x),
+                    Mathf.RoundToInt(position.y),
+                    Mathf.RoundToInt(position.z));
+            }
+
             Squares.AddRange(ActiveShape.Squares);
             CreateNewShape();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //TODO сделать проверку на границы
+            ActiveShape.transform.position += Vector3.left;
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            //TODO сделать проверку на границы
+            ActiveShape.transform.position += Vector3.right;
         }
     }
 
     public static bool CanMove(Shape shape, Vector3 newPosition, Field field)
     {
-        var currentPosition = shape.transform.position;
+        var lowerBound = newPosition.y + shape.MinY;
 
         //два условия остановки: нижняя граница поля, снизу есть клетки
         //lowerBondary
-        if (newPosition.y + shape.MinY < field.Position.y)
+        if (lowerBound < field.Position.y)
         {
             return false;
         }
 
+        //алгоритм
+        //пройтись по всем квадратам в фигуре и подсчитать его новые целочисленные координаты
+        //проверить если в таких координатах уже квадрат - если есть значит сказать что нельзя двигаться
+        foreach (var square in shape.Squares)
+        {
+            var position = new Vector3(
+                Mathf.RoundToInt(square.transform.localPosition.x + newPosition.x),
+                Mathf.RoundToInt(square.transform.localPosition.y + newPosition.y));
 
-        //hasSquares
+            var intersect =
+                field.Squares.Any(x => x.transform.position.y == position.y && x.transform.position.x == position.x);
+            if (intersect)
+            {
+                return false;
+            }
+        }
+
         return true;
     }
 }
